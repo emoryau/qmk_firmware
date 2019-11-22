5b6055324f4632c441c653164717f091e36d7586
emoryau <emoryau@gmail.com>
2019-11-22 11:08:59 -0500

    added printscreen key as alt layer delete

--- keyboards/keebio/quefrency/keymaps/emoryaumacro/keymap.c
+++ keyboards/keebio/quefrency/keymaps/emoryaumacro/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_FN1] = LAYOUT_65_with_macro(
-    KC_F11,    KC_F12,  KC_GRAVE,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC, KC_DEL, KC_INS, \
+    KC_F11,    KC_F12,  KC_GRAVE,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC, KC_PSCREEN, KC_INS, \
     _______,   _______, C(KC_GRAVE),RGB_TOG, RGB_MOD, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     _______,   _______, _______,    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     _______,   _______, KC_LSFT,    KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
